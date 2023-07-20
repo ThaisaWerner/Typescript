543c92d7c92887e0160f6926e8ecb5c32b969f5e
Thaisa Cristine de Souza Werner <thaisa.werner@thoughtworks.com>
2023-07-20 14:07:54 -0300

    TS Compiler

---
+++ TS compiler/dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const userName = 'Maximilian';
+console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+//# sourceMappingURL=app.js.map
